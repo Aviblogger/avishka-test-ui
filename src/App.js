5521324c137b2e41c98e949fd68d047ec3e92a7c
Aviblogger <avishkadevinda2015@gmail.com>
2022-05-07 11:58:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from './lib'
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          <Button label="ok learing now!"></Button>
         </a>
       </header>
     </div>
